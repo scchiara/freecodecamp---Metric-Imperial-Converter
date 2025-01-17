3827a34ac2f2628901011f157bf32146f7d5e18f
scchiara <chiara96_sc@libero.it>
2025-01-17 15:20:35 +0000

    Initial commit

--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,40 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
+
+    // invalid number and invalid unit
+    if(!initNum && !initUnit) {
+      res.send('invalid number and unit');
+      return;
+    }
+
+    // invalid number
+    if(!initNum) {
+      res.send('invalid number');
+      return;
+    }
+
+    //invalid unit
+    if(!initUnit) {
+      res.send('invalid unit');
+      return;
+    }
+
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let toString = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+
+    res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
+
+  });
+
 };
